3bed8b2191552de01640a9c39bd07c7608c8179c
Zeyu Yao <novodoodle@gmail.com>
2023-03-21 12:09:53 +0800

    Remove border on registration form

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,46 +1,59 @@
-import { useState, useCallback, useEffect, useRef } from 'react';
+import { useState, useCallback, useEffect, useRef } from "react";
+import styles from "@/styles/RegisterForm.module.css";
 
-const CODEDAY_ORIGIN = 'https://event.codeday.org';
+const CODEDAY_ORIGIN = "https://event.codeday.org";
 
 interface Props {
-  language?: string;
-  webname: string;
+    language?: string;
+    webname: string;
 }
 
-export default function CodeDayRegistrationForm({ language = 'en-US', webname }: Props) {
-  const ref = useRef<HTMLIFrameElement>(null);
-  const [height, setHeight] = useState<number>(400);
-  
-  // Validate that the message came from CodeDay
-  const onResizeMessageHandler = useCallback((e: MessageEvent) => {
-    if (e.origin !== CODEDAY_ORIGIN) return;
-    const { type, height } = JSON.parse(e.data);
-    if (type !== 'embedResize') return;
-    setHeight(height);
-  }, [setHeight]);
+export default function CodeDayRegistrationForm({
+    language = "en-US",
+    webname,
+}: Props) {
+    const ref = useRef<HTMLIFrameElement>(null);
+    const [height, setHeight] = useState<number>(400);
 
-  // When we receive a new size, update the state size.
-  const isWindowDefined = typeof window !== 'undefined';
-  useEffect(() => {
-    if (!isWindowDefined) return;
-    window.addEventListener('message', onResizeMessageHandler);
-    return () => window.removeEventListener('message', onResizeMessageHandler);
-  }, [isWindowDefined, onResizeMessageHandler]);
+    // Validate that the message came from CodeDay
+    const onResizeMessageHandler = useCallback(
+        (e: MessageEvent) => {
+            if (e.origin !== CODEDAY_ORIGIN) return;
+            const { type, height } = JSON.parse(e.data);
+            if (type !== "embedResize") return;
+            setHeight(height);
+        },
+        [setHeight]
+    );
 
-  // When this component first loads, request the size.
-  const isRefDefined = !!ref.current;
-  useEffect(() => {
-    if (!isRefDefined || !isWindowDefined) return;
-    ref.current?.contentWindow?.postMessage('poll', CODEDAY_ORIGIN);
-    ref.current?.addEventListener('load', () => ref.current?.contentWindow?.postMessage('poll', CODEDAY_ORIGIN));
-  }, [isRefDefined, isWindowDefined]);
+    // When we receive a new size, update the state size.
+    const isWindowDefined = typeof window !== "undefined";
+    useEffect(() => {
+        if (!isWindowDefined) return;
+        window.addEventListener("message", onResizeMessageHandler);
+        return () =>
+            window.removeEventListener("message", onResizeMessageHandler);
+    }, [isWindowDefined, onResizeMessageHandler]);
 
-  return (
-    <iframe
-      width="100%"
-      height={height}
-      ref={ref}
-      src={`${CODEDAY_ORIGIN}/${language}/${webname}/embed`}
-    />
-  );
+    // When this component first loads, request the size.
+    const isRefDefined = !!ref.current;
+    useEffect(() => {
+        if (!isRefDefined || !isWindowDefined) return;
+        ref.current?.contentWindow?.postMessage("poll", CODEDAY_ORIGIN);
+        ref.current?.addEventListener("load", () =>
+            ref.current?.contentWindow?.postMessage("poll", CODEDAY_ORIGIN)
+        );
+    }, [isRefDefined, isWindowDefined]);
+
+    return (
+        <div className={styles.registerForm}>
+            <iframe
+                width="100%"
+                height={height}
+                ref={ref}
+                src={`${CODEDAY_ORIGIN}/${language}/${webname}/embed`}
+                className={styles.registerForm__iframe}
+            />
+        </div>
+    );
 }
